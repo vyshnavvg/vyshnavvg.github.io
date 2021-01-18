2eec5dc3ce35ac52bd222dade38cb2d84e1bab2a
Vyshnav V G <72264417+vyshnavvg@users.noreply.github.com>
2021-01-18 09:53:38 +0530

    Add files via upload
    T3

---
+++ js/contact.js
@@ -0,0 +1,4 @@
+function check(){
+	alert("ERROR 404\n Currently the server is not pluged in. So contact through the social media handles. We apologize for the inconvenience caused.");
+	return false;
+}
